39799c0c1d99194401fbe8de2b81454ea20a3caf
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-02-12 20:15:18 +0300

    Update Main_regex_app1.py

--- Main_regex_app1.py
+++ Main_regex_app1.py
@@ -5,7 +5,7 @@ class RegexValidation:
         self.validation = validation
 
     def movie_titles(self):
-        #below
+        #Movie titles
         pattern = r"insert your movie title regex pattern here"
         movies = re.findall(pattern, self.validation)
         if movies is None or len(movies) == 0:
@@ -14,7 +14,7 @@ class RegexValidation:
             print(movies, end=",")
 
     def songs(self):
-         #below
+         #Songs
         pattern = r'^\[Verse/\d+].+$'
         songs = re.findall(pattern, self.validation)
         if songs is None or len(songs) == 0:
@@ -23,7 +23,7 @@ class RegexValidation:
             print(songs, end=",")
 
     def twitter(self):
-         #below
+         #Twitter Usernames
         pattern = r'^@[a-zA-Z0-9]+$'
         usernames = re.findall(pattern, self.validation)
         if usernames is None or len(usernames) == 0:
@@ -32,7 +32,7 @@ class RegexValidation:
             print(usernames, end=",")
 
     def isbn(self):
-         #below
+         #ISBN
         pattern = r"insert your ISBN regex pattern here"
         isbns = re.findall(pattern, self.validation)
         if isbns is None or len(isbns) == 0:
@@ -41,7 +41,7 @@ class RegexValidation:
             print(isbns, end=",")
 
     def jokes(self):
-         #below
+         #Jokes
         pattern = r"insert your joke regex pattern here"
         joke = re.findall(pattern, self.validation)
         if joke is None or len(joke) == 0:
@@ -50,7 +50,7 @@ class RegexValidation:
             print(joke, end=",")
 
     def tv(self):
-         #below
+         #TV Episodes
         pattern = r"insert your TV episode regex pattern here"
         episodes = re.findall(pattern, self.validation)
         if episodes is None or len(episodes) == 0:
@@ -59,7 +59,7 @@ class RegexValidation:
             print(episodes, end=",")
 
     def dates(self):
-         #below
+         #Weird Dates
         pattern = r"insert your date regex pattern here"
         dates = re.findall(pattern, self.validation)
         if dates is None or len(dates) == 0:
@@ -68,8 +68,8 @@ class RegexValidation:
             print(dates, end=",")
 
     def hex(self):
-         #below
-        pattern = r"insert your hex code regex pattern here"
+         #Hex Codes
+        pattern = r"#[a-zA-Z0-9]{6,8}"
         codes = re.findall(pattern, self.validation)
         if codes is None or len(codes) == 0:
             raise FileNotFoundError("None of these match")
@@ -77,8 +77,9 @@ class RegexValidation:
             print(codes, end=",")
 
     def ip(self):
-         #below
-        pattern = r"insert your IP address regex pattern here"
+         #IP address
+        pattern = r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$
+"
         addresses = re.findall(pattern, self.validation)
         if addresses is None or len(addresses) == 0:
             raise FileNotFoundError("None of these match")
